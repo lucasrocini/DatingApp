5be0c9236a0f8a6e88ed0064f6ad4841582352eb
Lucas <lucasrocini@hotmail.com>
2021-02-16 00:46:33 -0300

    Section 15 - Lesson 193

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -38,6 +38,8 @@ namespace API.Entities
         public ICollection<UserLike> LikedByUsers { get; set; }
 
         public ICollection<UserLike> LikedUsers { get; set; }
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
         
 
         // public int GetAge()
