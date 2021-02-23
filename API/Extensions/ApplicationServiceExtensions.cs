9cee2df34ce64b22e6bf853851685061ce2ada10
Lucas <lucasrocini@hotmail.com>
2021-02-23 16:58:22 -0300

    Heroku - problem solving

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -42,7 +42,7 @@ namespace API.Extensions
                     // Use connection string provided at runtime by Heroku.
                     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-                    // Parse connection URL to connection string for Npgsql
+                    // Parse connection URL to connection string for Npgsql 
                     connUrl = connUrl.Replace("postgres://", string.Empty);
                     var pgUserPass = connUrl.Split("@")[0];
                     var pgHostPortDb = connUrl.Split("@")[1];
